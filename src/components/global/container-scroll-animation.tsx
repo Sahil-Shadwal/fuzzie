8a61ecbd49db333b80e26231b2e659632b51fd51
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-08-15 11:54:33 +0530

    design fix

--- src/components/global/container-scroll-animation.tsx
+++ src/components/global/container-scroll-animation.tsx
@@ -31,7 +31,7 @@ export const ContainerScroll = ({
 
   const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
   const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
-  const translate = useTransform(scrollYProgress, [0, 1], [0, -100]);
+  const translate = useTransform(scrollYProgress, [0, 1], [0, -50]); // Reduced translate amount
 
   return (
     <div
@@ -78,12 +78,13 @@ export const Card = ({
       style={{
         rotateX: rotate, // rotate in X-axis
         scale,
+        translateY: translate, // Apply the translateY
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
-      className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full  p-6 bg-[#222222] rounded-[30px] shadow-2xl"
+      className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full p-6 bg-[#222222] rounded-[30px] shadow-2xl"
     >
-      <div className="bg-gray-100 h-full w-full rounded-2xl  gap-4 overflow-hidden p-4 transition-all ">
+      <div className="bg-gray-100 h-full w-full rounded-2xl gap-4 overflow-hidden p-4 transition-all">
         <Image
           src="/temp-banner.png"
           fill
