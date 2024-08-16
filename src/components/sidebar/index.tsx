56e817148010f9675f6ab7d8bec11cd96ff6f23f
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-08-16 15:01:17 +0530

    sidebar toggle fix

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -20,9 +20,9 @@ const MenuOptions = (props: Props) => {
   const pathName = usePathname();
 
   return (
-    <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
-      <div className="flex items-center justify-center flex-col gap-8">
-        <Link className="flex font-bold flex-row " href="/">
+    <nav className="dark:bg-black h-screen overflow-scroll flex items-center flex-col gap-10 py-6 px-2">
+      <div className="md:scale-75 lg:scale-100 flex items-center justify-center flex-col gap-8">
+        <Link className="flex font-bold flex-row" href="/">
           fuzzie.
         </Link>
         <TooltipProvider>
@@ -34,9 +34,9 @@ const MenuOptions = (props: Props) => {
                     <Link
                       href={menuItem.href}
                       className={clsx(
-                        "group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer",
+                        "group h-8 w-8 flex items-center justify-center scale-[1.5] rounded-lg p-[3px] cursor-pointer",
                         {
-                          "dark:bg-[#2F006B] bg-[#EEE0FF] ":
+                          "dark:bg-[#2F006B] bg-[#EEE0FF]":
                             pathName === menuItem.href,
                         }
                       )}
@@ -76,7 +76,7 @@ const MenuOptions = (props: Props) => {
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-center flex-col gap-8">
+      <div className="md:scale-75 lg:scale-100 flex items-center justify-center flex-col gap-8">
         <ModeToggle />
       </div>
     </nav>
